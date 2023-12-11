c6daa488cc3f84056e731304375c1c884ec1af90
aldisypu <aldisypu@gmail.com>
2023-12-11 15:49:41 +0700

    belajar golang restful api

---
+++ service/category_service.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"alsyodi/belajar-golang-restful-api/model/web"
+
+	"golang.org/x/net/context"
+)
+
+type CategoryService interface {
+	Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse
+	Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse
+	Delete(ctx context.Context, categoryId int)
+	FindById(ctx context.Context, categoryId int) web.CategoryResponse
+	FindAll(ctx context.Context) []web.CategoryResponse
+}
